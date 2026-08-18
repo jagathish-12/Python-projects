f6518b57892f02adbc41409902b606a0a909972f
Jagathish s <jagathishs229@gmail.com>
2026-08-18 10:41:59 +0530

    Band name Generator

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+#Stores the 2 variables and concatenate them in a sentence
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's the pet name? \n")
+print("Your band name could be "+city_name+" "+pet_name)
